f5e576b0812034f6a7f41f29da1b72674201430c
Ignacio Oliva Velez <nachoolivavelez18@gmail.com>
2024-06-09 13:28:16 -0300

    first commit

---
+++ src/main/java/com/uade/backendgestionbd2/BackendGestionBd2Application.java
@@ -0,0 +1,13 @@
+package com.uade.backendgestionbd2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendGestionBd2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendGestionBd2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/uade/backendgestionbd2/BackendGestionBd2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uade.backendgestionbd2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendGestionBd2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
