da6fda9685f9c2aa08306fa845cbd2a8c609a65b
Agustin Aused <agus.aused@gmail.com>
2024-06-11 14:42:18 -0300

    Agregados constructores a la entidad Proyectos.
    - Se han añadido constructores a la clase Proyectos para inicialización y creación de instancias.
    - Este commit mejora la estructura y funcionalidad de la entidad en la aplicación.
    
    Signed-off-by: Agustin Aused <agus.aused@gmail.com>

--- src/main/java/com/uade/backendgestionbd2/model/Projects.java
+++ src/main/java/com/uade/backendgestionbd2/model/Projects.java
@@ -4,23 +4,40 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-
 //Lombok
 @Getter
 @Setter
 @ToString
 
+
 @Entity
 public class Projects {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int project_id;
+    private int projedct_id;
 
     private String name;
+
     private String description;
+
     private String start_date;
+
     private String end_date;
+
     private String status;
+
+    public Projects() {
+    }
+
+    public Projects( String name, String description, String start_date, String end_date, String status) {
+        this.name = name;
+        this.description = description;
+        this.start_date = start_date;
+        this.end_date = end_date;
+        this.status = status;
+    }
+
+
 
 
 }
