ce627b143d777fed0fe8ac8008a31d7da3ec5bcd
Agustin Aused <agus.aused@gmail.com>
2024-06-14 12:54:40 -0300

    agregar datos de pruebas a las tablas cuando se crean
    Signed-off-by: Agustin Aused <agus.aused@gmail.com>

--- src/main/resources/db/init.sql
+++ src/main/resources/db/init.sql
@@ -1,12 +1,12 @@
 -- Crear la tabla de usuarios
 CREATE TABLE Users (
                        user_id SERIAL PRIMARY KEY,
-                       username VARCHAR(50) NOT NULL UNIQUE,
+                       username VARCHAR(50) NOT NULL,
                        password VARCHAR(100) NOT NULL,
                        role VARCHAR(50) NOT NULL CHECK (role IN ('Admin', 'Employee')),
                        name VARCHAR(100),
                        last_name VARCHAR(100),
-                       email VARCHAR(100),
+                       email VARCHAR(100) NOT NULL UNIQUE,
                        weekly_hours INT DEFAULT 0,
                        CONSTRAINT email_format CHECK (email ~* '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')
 );
@@ -55,29 +55,93 @@ CREATE TABLE ProjectAssignments (
 -- Insertar datos de prueba
 
 -- Insertar datos en Projects
-INSERT INTO Projects (name, description, start_date, end_date, weekly_hours) VALUES
-                                                                                 ('Project Alpha', 'Description for Project Alpha', '2024-01-01', '2024-12-31', 40),
-                                                                                 ('Project Beta', 'Description for Project Beta', '2024-02-01', '2024-11-30', 30);
+INSERT INTO Projects (name, description, start_date, end_date, weekly_hours)
+VALUES
+    ('Project Alpha', 'Description for Project Alpha', '2024-01-01', '2024-12-31', 40),
+    ('Project Beta', 'Description for Project Beta', '2024-02-01', '2024-11-30', 30),
+    ('Project Gamma', 'Description for Project Gamma', '2024-03-01', '2024-10-31', 25),
+    ('Project Delta', 'Description for Project Delta', '2024-04-01', '2024-09-30', 20),
+    ('Project Epsilon', 'Description for Project Epsilon', '2024-05-01', '2024-08-31', 15),
+    ('Project Zeta', 'Description for Project Zeta', '2024-06-01', '2024-07-31', 10),
+    ('Project Theta', 'Description for Project Theta', '2024-07-01', '2024-06-30', 5),
+    ('Project Iota', 'Description for Project Iota', '2024-08-01', '2024-05-31', 0),
+    ('Project Kappa', 'Description for Project Kappa', '2024-09-01', '2024-04-30', 0),
+    ('Project Lambda', 'Description for Project Lambda', '2024-10-01', '2024-03-31', 0);
 
--- Insertar datos en Users
-INSERT INTO Users (username, password, role, name, email, weekly_hours) VALUES
-                                                                            ('admin_user', 'admin_password', 'Admin', 'Admin User', 'admin@example.com', 40),
-                                                                            ('employee_user1', 'employee_password1', 'Employee', 'Employee User1', 'employee1@example.com', 35),
-                                                                            ('employee_user2', 'employee_password2', 'Employee', 'Employee User2', 'employee2@example.com', 30);
+
+-- Insertar datos en Users con contraseñas encriptadas en Base64
+INSERT INTO Users (username, password, role, name, last_name, email, weekly_hours)
+VALUES
+    ('admin_user', 'QWRtaW5fMTIzNDU2', 'Admin', 'Admin', 'User', 'admin@example.com', 40), -- Contraseña encriptada: Admin_123456
+    ('employee_user1', 'RW1wbG95ZWVfMTIzNDU2', 'Employee', 'Employee', 'User1', 'employee1@example.com', 35), -- Contraseña encriptada: Employee_123456
+    ('employee_user2', 'RW1wbG95ZWVfMTIzNDU2', 'Employee', 'Employee', 'User2', 'employee2@example.com', 30), -- Contraseña encriptada: Employee_123456
+    ('user3', 'VXNlcm5hbWVfMTIzNDU2', 'Employee', 'User', 'Three', 'user3@example.com', 25), -- Contraseña encriptada: Username_123456
+    ('user4', 'VXNlcm5hbWVfMTIzNDU2', 'Employee', 'User', 'Four', 'user4@example.com', 20), -- Contraseña encriptada: Username_123456
+    ('user5', 'UGFzc3dvcmRfMTIzNDU2', 'Employee', 'User', 'Five', 'user5@example.com', 15), -- Contraseña encriptada: Password_123456
+    ('user6', 'UGFzc3dvcmRfMTIzNDU2', 'Employee', 'User', 'Six', 'user6@example.com', 10), -- Contraseña encriptada: Password_123456
+    ('user7', 'UGFzc3dvcmRfMTIzNDU2', 'Employee', 'User', 'Seven', 'user7@example.com', 5), -- Contraseña encriptada: Password_123456
+    ('user8', 'UGFzc3dvcmRfMTIzNDU2', 'Employee', 'User', 'Eight', 'user8@example.com', 0), -- Contraseña encriptada: Password_123456
+    ('user9', 'UGFzc3dvcmRfMTIzNDU2', 'Employee', 'User', 'Nine', 'user9@example.com', 0); -- Contraseña encriptada: Password_123456
+
 
 -- Insertar datos en Tasks
-INSERT INTO Tasks (project_id, name, description, status, start_date, end_date) VALUES
-                                                                                    (1, 'Task 1', 'Description for Task 1', 'In Activities', '2024-01-01', '2024-06-30'),
-                                                                                    (1, 'Task 2', 'Description for Task 2', 'To Do', '2024-02-01', '2024-07-31'),
-                                                                                    (2, 'Task 3', 'Description for Task 3', 'Done', '2024-03-01', '2024-05-31');
+-- Insertar datos en Tasks para Project Alpha
+INSERT INTO Tasks (project_id, name, description, status, start_date, end_date)
+VALUES
+    (1, 'Task 1 for Project Alpha', 'Description for Task 1', 'In Activities', '2024-01-01', '2024-06-30'),
+    (1, 'Task 2 for Project Alpha', 'Description for Task 2', 'To Do', '2024-02-01', '2024-07-31'),
+    (1, 'Task 3 for Project Alpha', 'Description for Task 3', 'Done', '2024-03-01', '2024-05-31');
+
+-- Insertar datos en Tasks para Project Beta
+INSERT INTO Tasks (project_id, name, description, status, start_date, end_date)
+VALUES
+    (2, 'Task 1 for Project Beta', 'Description for Task 1', 'In Activities', '2024-02-01', '2024-07-31'),
+    (2, 'Task 2 for Project Beta', 'Description for Task 2', 'To Do', '2024-03-01', '2024-08-31'),
+    (2, 'Task 3 for Project Beta', 'Description for Task 3', 'Done', '2024-04-01', '2024-09-30');
+
+-- Insertar datos en Tasks para Project Gamma
+INSERT INTO Tasks (project_id, name, description, status, start_date, end_date)
+VALUES
+    (3, 'Task 1 for Project Gamma', 'Description for Task 1', 'In Activities', '2024-03-01', '2024-10-31'),
+    (3, 'Task 2 for Project Gamma', 'Description for Task 2', 'To Do', '2024-04-01', '2024-11-30'),
+    (3, 'Task 3 for Project Gamma', 'Description for Task 3', 'Done', '2024-05-01', '2024-12-31');
+
+-- Insertar más datos en Tasks para Project Delta
+INSERT INTO Tasks (project_id, name, description, status, start_date, end_date)
+VALUES
+    (4, 'Task 1 for Project Delta', 'Description for Task 1', 'To Do', '2024-07-01', '2024-08-31'),
+    (4, 'Task 2 for Project Delta', 'Description for Task 2', 'In Activities', '2024-08-01', '2024-09-30'),
+    (4, 'Task 3 for Project Delta', 'Description for Task 3', 'Done', '2024-09-01', '2024-10-31');
+
+-- Insertar más datos en Tasks para Project Epsilon
+INSERT INTO Tasks (project_id, name, description, status, start_date, end_date)
+VALUES
+    (5, 'Task 1 for Project Epsilon', 'Description for Task 1', 'To Do', '2024-08-01', '2024-09-30'),
+    (5, 'Task 2 for Project Epsilon', 'Description for Task 2', 'In Activities', '2024-09-01', '2024-10-31'),
+    (5, 'Task 3 for Project Epsilon', 'Description for Task 3', 'Done', '2024-10-01', '2024-11-30');
+
+-- Insertar más datos en Tasks para Project Zeta
+INSERT INTO Tasks (project_id, name, description, status, start_date, end_date)
+VALUES
+    (6, 'Task 1 for Project Zeta', 'Description for Task 1', 'To Do', '2024-09-01', '2024-10-31'),
+    (6, 'Task 2 for Project Zeta', 'Description for Task 2', 'In Activities', '2024-10-01', '2024-11-30'),
+    (6, 'Task 3 for Project Zeta', 'Description for Task 3', 'Done', '2024-11-01', '2024-12-31');
 
 -- Insertar datos en TaskAssignments
-INSERT INTO TaskAssignments (task_id, user_id) VALUES
-                                                   (1, 3),
-                                                   (2, 3),
-                                                   (3, 3);
+INSERT INTO TaskAssignments (task_id, user_id)
+VALUES
+    (1, 2), -- Asignar Task 1 del proyecto Alpha al usuario employee_user1
+    (2, 3), -- Asignar Task 2 del proyecto Alpha al usuario employee_user2
+    (3, 3), -- Asignar Task 3 del proyecto Beta al usuario employee_user2
+    (4, 1), -- Asignar Task 1 del proyecto Delta al usuario admin_user
+    (5, 2), -- Asignar Task 1 del proyecto Epsilon al usuario employee_user1
+    (6, 3); -- Asignar Task 1 del proyecto Zeta al usuario employee_user2
 
 -- Insertar datos en ProjectAssignments
-INSERT INTO ProjectAssignments (project_id, user_id) VALUES
-                                                         (1, 3),
-                                                         (2, 3);
+INSERT INTO ProjectAssignments (project_id, user_id)
+VALUES
+    (1, 3), -- Asignar Project Alpha al usuario employee_user2
+    (2, 3), -- Asignar Project Beta al usuario employee_user2
+    (4, 1), -- Asignar Project Delta al usuario admin_user
+    (5, 2), -- Asignar Project Epsilon al usuario employee_user1
+    (6, 3); -- Asignar Project Zeta al usuario employee_user2
