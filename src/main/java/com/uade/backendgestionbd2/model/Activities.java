1a7eea4935b0aae5897253a0932449bb2968d5cf
Agustin Aused <agus.aused@gmail.com>
2024-06-14 18:12:43 -0300

    modificar modelos noSql
    Signed-off-by: Agustin Aused <agus.aused@gmail.com>

--- src/main/java/com/uade/backendgestionbd2/model/Activities.java
+++ src/main/java/com/uade/backendgestionbd2/model/Activities.java
@@ -1,28 +1,39 @@
 package com.uade.backendgestionbd2.model;
 
-import jakarta.persistence.Id;
-import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Date;
 
+@Getter
+@Setter
+@ToString
 @Document(collection = "activities")
 public class Activities {
 
     @Id
-    private String progress_id;
+    private String Activity_id;
 
     private String task_id;
-
-    @DBRef
-    private Set<String> updates_ids = new HashSet<>();
+    private String user_id;
+    private String description;
+    private Date timestamp;
+    private int progress_percentage;
+    private int time_worked;
 
     public Activities() {
     }
 
-    public Activities(String task_id) {
+    public Activities(String task_id, String user_id, String description, Date timestamp, int progress_percentage, int time_worked) {
         this.task_id = task_id;
+        this.user_id = user_id;
+        this.description = description;
+        this.timestamp = timestamp;
+        this.progress_percentage = progress_percentage;
+        this.time_worked = time_worked;
     }
-
 }
