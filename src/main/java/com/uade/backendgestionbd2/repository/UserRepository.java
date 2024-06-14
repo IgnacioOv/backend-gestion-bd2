91641d8c862fb7b62a907536843075e6885db41d
Agustin Aused <agus.aused@gmail.com>
2024-06-14 14:33:45 -0300

    Añadidos métodos personalizados en UserRepository: - Método findByEmail para buscar usuarios por correo electrónico. - Método findUsersByProjectId para obtener usuarios asignados a un proyecto específico. - Método findUsersByTaskId para obtener usuarios asignados a una tarea específica.
    Signed-off-by: Agustin Aused <agus.aused@gmail.com>

--- src/main/java/com/uade/backendgestionbd2/repository/UserRepository.java
+++ src/main/java/com/uade/backendgestionbd2/repository/UserRepository.java
@@ -2,12 +2,22 @@ package com.uade.backendgestionbd2.repository;
 
 import com.uade.backendgestionbd2.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<Users, Integer> {
     // Métodos personalizados si es necesario
+    @Query("SELECT u FROM Users u WHERE u.email = :email")
     Optional<Users> findByEmail(String email);
+
+    @Query("SELECT u FROM Users u JOIN ProjectsAssignments pa ON u.user_id = pa.user_id WHERE ta.project_id = :projectId")
+    Optional<List<Users>> findUsersByProjectId(int projectId);
+
+    @Query("SELECT u FROM Users u JOIN u.tasks t WHERE t.task_id = :taskId")
+    Optional<List<Users>> findUsersByTaskId(int taskId);
+
 }
