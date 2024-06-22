6a5a08acdd21f5d9b3d34d0a9d74c26179d94f30
Ignacio Oliva Velez <nachoolivavelez18@gmail.com>
2024-06-22 20:33:19 -0300

    feat: adds project assignment endpoints and modifies updateassignment service

---
+++ src/main/java/com/uade/backendgestionbd2/dto/ProjectAssignmentDto.java
@@ -0,0 +1,12 @@
+package com.uade.backendgestionbd2.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ProjectAssignmentDto {
+    private int projectId;
+    private int userId;
+}
